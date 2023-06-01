3ccf400c0acd3b956e9c39670e309a32be34682f
Luis Oliverez <louoliverez901@gmail.com>
2023-06-01 14:44:20 -0700

    merging

---
+++ src/ContactsApp.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ContactsApp extends ContactMethods{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String choice;
+
+        ContactMethods contactMethods = new ContactMethods();
+
+        ArrayList<Contactee> contactlist = new ArrayList<>();
+
+        Contactee tyresz = new Contactee("tyresz","01000101001");
+        contactlist.add(tyresz);
+
+        Contactee luis = new Contactee("Luis", "010001010111");
+        contactlist.add(luis);
+
+
+//        for (Contactee contactee : contactlist) {
+//            printContacts(contactee);
+//        }
+
+
+
+        do {
+            System.out.println("1. View Contacts");
+//            System.out.println("2. Add new contact");
+//            System.out.println("3. Search a contact by name");
+//            System.out.println("4. Delete an existing contact");
+            System.out.println("5. Exit");
+            System.out.println(" Enter an option (1, 2, 3, 4, or 5): ");
+            choice = sc.nextLine();
+
+            switch (choice) {
+                case "1" -> System.out.println(contactlist);//VIEW CONTACTS
+//                case "2" -> {}//ADD NEW CONTACT
+//                case "3" -> System.out.println("Exiting the application. Have a good day....I SAID GOOD DAY!");
+//                case "4" -> {}//Delete an existing contact
+                case "5"->{} //Exit
+
+            }
+            System.out.println();
+
+        } while (!choice.equals("5"));
+    }
+
+    }
+
